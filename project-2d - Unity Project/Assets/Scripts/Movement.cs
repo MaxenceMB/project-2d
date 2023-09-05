40ea4670c5c9e6ce99e970226646131320eb69a1
Maxence <133963749+MaxenceMB@users.noreply.github.com>
2023-09-05 21:00:18 +0200

    Clean movement + Collisions

--- project-2d - Unity Project/Assets/Scripts/Movement.cs
+++ project-2d - Unity Project/Assets/Scripts/Movement.cs
@@ -4,33 +4,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-    public Rigidbody2D rigidBody;
-    public float speed = 0.05f;
-    float direction;
-
-    void Start(){
-        float direction = 0;
-    }
+    public Rigidbody2D rb;
+    public float runSpeed;
 
     void FixedUpdate(){
-        direction = 0;
-        if (Input.GetKey(KeyCode.W)){
-            rigidBody.velocity = new Vector2(0, speed * direction);
-        } 
-        if (Input.GetKey(KeyCode.S)){
-            rigidBody.velocity = new Vector2(0, -speed);
-        } 
-        if (Input.GetKey(KeyCode.A)){
-            rigidBody.velocity = new Vector2(-speed, 0);
-        } 
-        if (Input.GetKey(KeyCode.D)){
-            rigidBody.velocity = new Vector2(-speed, 0);
-        } 
+        float directionX = Input.GetAxisRaw("Horizontal");
+        float directionY = Input.GetAxisRaw("Vertical");
 
-        /*
-        float directionX = Input.GetAxis("Horizontal");
-        directionX = Mathf.Round(directionX);
-        rigidBody.velocity = new Vector2(speed * directionX, rigidBody.velocity.y);*/
+        rb.velocity = new Vector2(runSpeed * directionX, rb.velocity.y);
+        rb.velocity = new Vector2(rb.velocity.x, runSpeed * directionY);
     }
 
 }
