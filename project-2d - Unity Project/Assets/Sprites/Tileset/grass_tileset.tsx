0f58d2f1de33b9bc25ab548b2b729cc2fd896ec2
Maxence <133963749+MaxenceMB@users.noreply.github.com>
2023-09-05 15:28:19 +0200

    Grass Tileset

---
+++ project-2d - Unity Project/Assets/Sprites/Tileset/grass_tileset.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="grass_tileset" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="tilesetGrass.png" width="320" height="320"/>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="12.0029" width="10" height="19.9971"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="22" y="12" width="10" height="19.9971"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="-0.00390625" width="10" height="32.0049"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="22" y="0" width="10" height="32.0049"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="11.9982"/>
+   <object id="2" x="0" y="12" width="32" height="20"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="12" width="32" height="20"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="12" width="32" height="20"/>
+   <object id="3" x="22" y="0" width="10" height="11.9982"/>
+  </objectgroup>
+ </tile>
+</tileset>
