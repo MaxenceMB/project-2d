0ca082d9fc74a5772a376d619a223807c2b2e78b
Maxence <maury.maxence13@gmail.com>
2023-09-06 21:57:30 +0200

    Tileset Update

--- project-2d - Unity Project/Assets/Sprites/Tileset/grass_tileset.tsx
+++ project-2d - Unity Project/Assets/Sprites/Tileset/grass_tileset.tsx
@@ -1,24 +1,46 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="grass_tileset" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="tilesetGrass.png" width="320" height="320"/>
- <tile id="11">
+<tileset version="1.10" tiledversion="1.10.1" name="grass_tileset" tilewidth="32" tileheight="32" tilecount="100" columns="9">
+ <image source="tilesetGrass.png" width="288" height="320"/>
+ <tile id="10">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="12.0029" width="10" height="19.9971"/>
+   <object id="1" x="0.00390625" y="0.00390625" width="10" height="32.0049"/>
+   <object id="2" x="10" y="0" width="22" height="5.00425"/>
   </objectgroup>
  </tile>
- <tile id="13">
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="5.00425"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="22" y="0" width="10" height="32.0049"/>
+   <object id="2" x="0" y="0" width="22" height="5.00425"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
   <objectgroup draworder="index" id="2">
    <object id="1" x="22" y="12" width="10" height="19.9971"/>
   </objectgroup>
  </tile>
- <tile id="21">
+ <tile id="15">
   <objectgroup draworder="index" id="2">
-   <object id="2" x="0" y="-0.00390625" width="10" height="32.0049"/>
+   <object id="1" x="0" y="12" width="32" height="20"/>
   </objectgroup>
  </tile>
- <tile id="22">
+ <tile id="16">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="1" x="0" y="12" width="10" height="19.9971"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="32.0049"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="22" y="0" width="10" height="32.0049"/>
   </objectgroup>
  </tile>
  <tile id="23">
@@ -26,21 +48,46 @@
    <object id="1" x="22" y="0" width="10" height="32.0049"/>
   </objectgroup>
  </tile>
- <tile id="31">
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="32.0049"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="10" height="11.9982"/>
    <object id="2" x="0" y="12" width="32" height="20"/>
   </objectgroup>
  </tile>
- <tile id="32">
+ <tile id="29">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="12" width="32" height="20"/>
   </objectgroup>
  </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="12" width="32" height="20"/>
+   <object id="2" x="22" y="0" width="10" height="11.9982"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="22" y="0" width="10" height="5.00425"/>
+  </objectgroup>
+ </tile>
  <tile id="33">
   <objectgroup draworder="index" id="2">
-   <object id="2" x="0" y="12" width="32" height="20"/>
-   <object id="3" x="22" y="0" width="10" height="11.9982"/>
+   <object id="1" x="0" y="0" width="32" height="5.00425"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="5.00425"/>
   </objectgroup>
  </tile>
 </tileset>
