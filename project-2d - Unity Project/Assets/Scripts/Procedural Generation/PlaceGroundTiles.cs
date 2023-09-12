1836ee7cf4104103d374010c9e13167ac30ccce1
MoukhtarZBV <144065429+MoukhtarZBV@users.noreply.github.com>
2023-09-12 18:05:32 +0200

    Land generation with tile rules

--- project-2d - Unity Project/Assets/Scripts/Procedural Generation/PlaceGroundTiles.cs
+++ project-2d - Unity Project/Assets/Scripts/Procedural Generation/PlaceGroundTiles.cs
@@ -6,7 +6,8 @@ using UnityEngine.Tilemaps;
 public class PlaceGroundTiles : MonoBehaviour {
 
     public Tilemap tileMap;
-    public Tile[] tiles;
+    public RuleTile tileRule;
+    public Tile tile;
     public MapGenerator mapGenerator;
 
     public void Start(){
@@ -24,9 +25,9 @@ public class PlaceGroundTiles : MonoBehaviour {
         for (int x = 0; x < mapGenerator.width; x++){
             for (int y = 0; y < mapGenerator.height; y++){
                 if (mapGenerator.map[x, y] == 0){
-                    tileMap.SetTile(new Vector3Int(x, y, 0), tiles[0]);
+                    tileMap.SetTile(new Vector3Int(x, y, 0), tileRule);
                 } else {
-                    tileMap.SetTile(new Vector3Int(x, y, 0), tiles[1]);
+                    tileMap.SetTile(new Vector3Int(x, y, 0), tile);
                 }
             }
         }
