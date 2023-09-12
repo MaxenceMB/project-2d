552e5b5484333d2a297afc7a62c2537f6d4ef337
Maxence <133963749+MaxenceMB@users.noreply.github.com>
2023-09-12 09:02:51 +0200

    Interactible behabing correctly + sprites

--- project-2d - Unity Project/Assets/Scripts/PlayerInteractions.cs
+++ project-2d - Unity Project/Assets/Scripts/PlayerInteractions.cs
@@ -1,52 +1,29 @@
 using UnityEngine;
 
-enum InteractCases { None, Pnj, Chest }
 public class PlayerInteractions : MonoBehaviour {
 
     private bool selected = false;                          // True when the player is in a zone which he can interact with
-    private InteractCases interact = InteractCases.None;    // Keep in memory which type of interaction the player can have
+    private InteractibleBehaviour lastEncountered;
 
 
     void Update() {
         // Checks if the player is in the zone of an interactible object
-        if(selected && this.interact != InteractCases.None) {
+        if(selected) {
 
             // If the player presses 'E', the correct interaction starts
             if(Input.GetKeyDown(KeyCode.E)) {
-                StartInteraction();
+                lastEncountered.Interact();
             }
-        }
-    }
-
-    // Starts the corrects 
-    private void StartInteraction() {
-        switch(this.interact) {
-            case InteractCases.Pnj:
-                Debug.Log("Interacted with a NPC!");
-                break;
-
-            case InteractCases.Chest:
-                Debug.Log("Interacted with a Chest!");
-                break;
         }
     }
 
     // Checks trigger entrances
     void OnTriggerEnter2D(Collider2D other) {
-        this.selected = true;
-
-        switch(other.gameObject.tag) {
-            case "interact_PNJ":
-                this.interact = InteractCases.Pnj;
-                break;
-
-            case "interact_Chest":
-                this.interact = InteractCases.Chest;
-                break;
-
-            default:
-                this.interact = InteractCases.None;
-                break;
+        if(other.gameObject.tag == "Interactible") {
+            this.selected = true;
+            if(other.gameObject.GetComponent<InteractibleBehaviour>() != null) {
+                this.lastEncountered = other.gameObject.GetComponent<InteractibleBehaviour>();
+            }
         }
     }
 
