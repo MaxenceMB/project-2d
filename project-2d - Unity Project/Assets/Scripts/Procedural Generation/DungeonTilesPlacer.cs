c657b7e09b1baa7c05b8c27e7934c842e07adf43
MoukhtarZBV <144065429+MoukhtarZBV@users.noreply.github.com>
2023-09-21 14:55:32 +0200

    Dungeon generation core

---
+++ project-2d - Unity Project/Assets/Scripts/Procedural Generation/DungeonTilesPlacer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DungeonTilesPlacer : MonoBehaviour {
+
+    public Tilemap tileMap;
+    public RuleTile tileRule;
+    public Tile tile;
+    public DungeonMapGenerator mapGenerator;
+
+    public void Start(){
+        StartCoroutine(PlaceTiles());
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.R)){
+            StartCoroutine(PlaceTiles());
+        }
+    }
+
+    IEnumerator PlaceTiles(){
+        yield return null;
+        for (int x = 0; x < mapGenerator.width; x++){
+            for (int y = 0; y < mapGenerator.height; y++){
+                if (mapGenerator.map[x, y] == 0){
+                    tileMap.SetTile(new Vector3Int(x, y, 0), tileRule);
+                } else {
+                    tileMap.SetTile(new Vector3Int(x, y, 0), tile);
+                }
+            }
+        }
+    }
+
+}
